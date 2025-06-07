7b81278ef3c04a364bfc1ed46b9204da36e993a5
SigPresidente <andrea.poletti.94@gmail.com>
2025-06-07 15:05:54 +0200

    B: corrected main.py, now open and closes window correctly

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def main() :
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while () :
+    while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
-        pygame.Surface.fill(color = black)
+
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__" :
