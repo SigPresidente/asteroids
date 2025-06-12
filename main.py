c8a2bf001e3b45991340fc172d29b3be148519de
SigPresidente <andrea.poletti.94@gmail.com>
2025-06-12 07:10:30 +0200

    D: added movements and containers for objects

--- main.py
+++ main.py
@@ -10,19 +10,33 @@ def main() :
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containters = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
+
+    dt = 0
 
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable :
+            obj.draw(screen)
+            
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        
 
 if __name__ == "__main__" :
     main()
